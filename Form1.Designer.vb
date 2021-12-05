7a9b5697b67b46d9018525ea33beb1bacad1f949
Alias: David <88350777+Rythorian77@users.noreply.github.com>
2021-12-04 21:36:12 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Disable = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Disable
+        '
+        Me.Disable.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Disable.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Disable.ForeColor = System.Drawing.Color.Lime
+        Me.Disable.Location = New System.Drawing.Point(85, 12)
+        Me.Disable.Name = "Disable"
+        Me.Disable.Size = New System.Drawing.Size(122, 33)
+        Me.Disable.TabIndex = 2
+        Me.Disable.Text = "Mic off"
+        Me.Disable.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Lime
+        Me.Button3.Location = New System.Drawing.Point(85, 60)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(122, 33)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Instructions"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 105)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(273, 277)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(297, 405)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Disable)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Search Ultima"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Disable As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports System.Globalization
+Imports System.IO
+Imports System.Speech.Recognition
+Imports System.Text
+Imports System.Threading
+
+Public Class Form1
+
+    Private ReadOnly Drone As New SpeechRecognitionEngine
+    Private ReadOnly Qa As New DictationGrammar
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If Not Directory.Exists($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\WM") Then
+            Directory.CreateDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\WM")
+        End If
+        'My.Computer.FileSystem.CreateDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\WM")
+
+        Thread.CurrentThread.CurrentUICulture = New CultureInfo("en-US")
+
+        Drone.LoadGrammarAsync(Qa)
+        Drone.RequestRecognizerUpdate()
+        Drone.SetInputToDefaultAudioDevice()
+        Drone.InitialSilenceTimeout = TimeSpan.FromSeconds(2.5)
+        Drone.BabbleTimeout = TimeSpan.FromSeconds(1.5)
+        Drone.EndSilenceTimeout = TimeSpan.FromSeconds(1.2)
+        Drone.EndSilenceTimeoutAmbiguous = TimeSpan.FromSeconds(1.5)
+        AddHandler Drone.SpeechRecognized, AddressOf Recognizer_SpeechRecognized
+
+        Drone.RecognizeAsync(RecognizeMode.Multiple)
+
+        'In the event of duplicated word is in the command list >> (ArchiveData.txt), this removes double words
+        'Dim path As String = "C:\Users\justin.ross\source\repos\Search Ultima\Search Ultima\ArchiveData.txt"
+        'Dim lines As New HashSet(Of String)()
+        'Read to file
+        'Using sr As New StreamReader(path)
+        'Do While sr.Peek() >= 0
+        'lines.Add(sr.ReadLine())
+        'Loop
+        'End Using
+
+        'Write to file
+        'Using sw As New StreamWriter(path)
+        'For Each line As String In lines
+        'sw.WriteLine(line)
+        'Next
+        'End Using
+
+    End Sub
+
+    Private Sub Recognizer_SpeechRecognized(sender As Object, e As SpeechRecognizedEventArgs)
+        Dim speech As String = e.Result.Text.ToString
+
+        Select Case (speech)
+            Case speech
+
+                If speech <> "+" Then
+
+                    'This section will take spoken word, write to file then execute search.
+                    Dim sb As New StringBuilder
+
+                    sb.AppendLine(speech)
+                    File.WriteAllText("C:\Users\justin.ross\source\repos\Search Ultima\Search Ultima\File.txt", sb.ToString())
+                    speech = "https://www.youtube.com/search?q=" & Uri.EscapeUriString(speech)
+                    Dim proc As New Process()
+                    Dim startInfo As New ProcessStartInfo(speech)
+                    proc.StartInfo = startInfo
+                    proc.Start()
+
+                    Return
+                End If
+        End Select
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        'Create Event
+    End Sub
+
+    'Disable Engine
+    Private Sub Disable_Click(sender As Object, e As EventArgs) Handles Disable.Click
+        Drone.RecognizeAsyncStop()
+        Dim frm As New ScarlettUltima
+        frm.Show()
+        Hide()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        My.Computer.Audio.Play(My.Resources.informative, AudioPlayMode.Background)
+    End Sub
+
+End Class
